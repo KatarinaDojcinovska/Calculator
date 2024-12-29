0cf3565f98386b6bf48b7c2c5d97bb23bd97a35d
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2024-12-29 13:01:55 +0100

    Creating: function calculate
    - try{}:
    Try this code and "catch" any error
    
    -display.value:
    the value display.value means the value thats in input on the display.
    
    -eval(display.value)
    eval is a function that takes an expression and calculates it.
    Example eval(1+2+3)
    
    -catch (error) {}:
    It catches a error and puts error in the display

--- calc.js
+++ calc.js
@@ -2,3 +2,12 @@ const display = document.getElementById("display");
 function appendToDisplay (input){
     display.value += input;
 }
+function calculate ();
+{
+    try{
+        display.value = eval(display.value);
+    }
+    catch (error){
+        display.value = "Error";
+    }
+}
